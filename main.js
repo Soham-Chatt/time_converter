64c05d12a2733f527133c10aaf138ef657b45cc6
Soham-Chatt <113625563+Soham-Chatt@users.noreply.github.com>
2023-05-27 22:03:26 +0200

    Time conversion only shows times that are not zero

--- main.js
+++ main.js
@@ -1,32 +1,12 @@
 // main.js
 
-// Function to clear all input values on page load
+// Function to clear all input values on page load and display the current time
 window.onload = function() {
     document.getElementById("secondsInput").reset();
+    displayTime();
 }
 
-// Function to convert seconds to days, hours, minutes, and seconds
-document.getElementById("secondsInput").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const form = document.getElementById("secondsInput");
-    const totalSeconds = Number(form.seconds.value);
-    const excludeDays = form.days.checked;
-    const target = document.querySelector(".result");
-
-    let days = Math.floor(totalSeconds / (3600*24));
-    let hours = Math.floor(totalSeconds % (3600*24) / 3600);
-    let minutes = Math.floor((totalSeconds % 3600) / 60);
-    let seconds = Math.floor(totalSeconds % 60);
-
-    if (excludeDays) {
-        hours += days * 24;
-        target.innerHTML = `<h4 class="text-primary">${hours}h ${minutes}m ${seconds}s</h4>`;
-    } else {
-        target.innerHTML = `<h4 class="text-primary">${days}d ${hours}h ${minutes}m ${seconds}s</h4>`;
-    }
-
-});
+setInterval(displayTime, 1000); // Update the time every second
 
 // Function to get the current time and display it on the page every second
 function displayTime() {
@@ -35,10 +15,30 @@ function displayTime() {
     clock.innerHTML = `<h4 class="clock">${time}</h4>`;
 }
 
-// Call the displayTime function initially to show the time immediately
-displayTime();
+// Function to convert seconds to days, hours, minutes, and seconds
+document.getElementById("secondsInput").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-// Call the displayTime function every second using setInterval
-setInterval(displayTime, 1000);
+    // Get known values from the form
+    const excludeDays = event.target.days.checked;
+    const target = document.querySelector(".result");
+    let seconds = Number(event.target.seconds.value);
+    let resultStr = '<h4 class="text-primary">';
 
+    // Calculate the appropriate number of days, hours, minutes, and seconds
+    const days = Math.floor(seconds / 86400);
+    seconds %= 86400;
+    const hours = excludeDays ? Math.floor((days * 24) + seconds / 3600) : Math.floor(seconds / 3600);
+    seconds %= 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+    seconds = Math.floor(seconds);
 
+    // Build the result string with the necessary values
+    if (!excludeDays && days > 0) resultStr += `${days}d `; // Days
+    if (hours > 0) resultStr += `${hours}h `; // Hours
+    if (minutes > 0) resultStr += `${minutes}m `; // Minutes
+    resultStr += `${seconds}s</h4>`; // Seconds will always be displayed
+
+    target.innerHTML = resultStr;
+});
